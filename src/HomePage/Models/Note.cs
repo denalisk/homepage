402cded2fdc3fc8469e7be0dcde87558741980eb
Sam Kirsch <samdkirsch@gmail.com>
2017-05-10 11:42:11 -0700

    create initial files and repositories

---
+++ src/HomePage/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomePage.Models
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ src/HomePage/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomePage.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
